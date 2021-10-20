a28a3c27757b2017269fd1ce39215e75518b9624
nupdoong <37859149+nupdoong@users.noreply.github.com>
2021-10-20 15:39:38 +0900

    Delete PortfolioSharingService/src/src directory

--- PortfolioSharingService/src/src/main/java/com/portfolio/security/repository/SaltRepository.java
+++
@@ -1,8 +0,0 @@
-package com.portfolio.security.repository;
-
-import com.portfolio.security.model.Salt;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SaltRepository extends CrudRepository<Salt,Long> {
-
-}
--- PortfolioSharingService/src/src/main/java/com/portfolio/security/service/EmailService.java
+++
@@ -1,6 +0,0 @@
-package com.portfolio.security.service;
-
-public interface EmailService {
-
-    void sendMail(String to, String sub, String text);
-}
