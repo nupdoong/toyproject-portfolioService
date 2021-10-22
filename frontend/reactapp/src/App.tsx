61d54bf9956f8554610f95546df61b37f6559bed
nupdoong <37859149+nupdoong@users.noreply.github.com>
2021-10-22 13:15:45 +0900

    LoginPage 파일명 수정, MainPage, MyPortfolioPage 파일 생성

--- frontend/reactapp/src/App.tsx
+++ frontend/reactapp/src/App.tsx
@@ -1,16 +1,17 @@
 import * as React from "react";
 import logo from './logo.svg';
 import {Link, Route, BrowserRouter as Router} from "react-router-dom";
-import {MainPage,SignUpPage, SocialLoginingPage, MyPortfolioPage} from "./Container";
+import {MainPage, LoginPage, SignUpPage, SocialLoginingPage, MyPortfolioPage} from "./Container";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Route path="/" exact component={MainPage}/>
+        <Route path="/lgoin" exact component={LoginPage}/>
         <Route path="/signup" component={SignUpPage}/>
         <Route path="/login/oauth" component={SocialLoginingPage}/>
-        <Route path="/mypofol" component={MyPortfolioPage}/>
+        <Route path="/myportfolio" component={MyPortfolioPage}/>
       </Router>
     </div>
   );
