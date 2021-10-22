f7cd09018b70e94f40755f600c4066911cd8820b
nupdoong <37859149+nupdoong@users.noreply.github.com>
2021-10-22 13:25:57 +0900

    MyPortfolioPresenter, MainPresenter 생성

---
+++ frontend/reactapp/src/Presenter/MyPortfolio/MyPortfolioPresenter.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import "./MyPortfolioPresenter.scss";
+import { Button, Input, Form } from "antd";
+
+const MyPortfolioContainer = () => {
+    
+    return(
+        <div>
+        </div>
+    )
+
+}
+
+export default MyPortfolioContainer;
